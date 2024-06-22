ff46b3da6cd457b990f1a55611bff9facb0787ad
Kageetai <ma@kageetai.net>
2024-06-22 14:12:32 +0200

    add Mastodon link
    no icon for external links

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -8,6 +8,7 @@ export const sharedPageComponents: SharedLayout = {
   footer: Component.Footer({
     links: {
       GitHub: "https://github.com/kageetai/kageetai.net",
+      Mastodon: "https://mastodon.social/@kageetai",
     },
   }),
 }
