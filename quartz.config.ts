59d0a3e27ad6878abdc7a8766b42630895e3131f
Kageetai <ma@kageetai.net>
2025-10-21 17:37:43 +0200

    remove CrawlLinks from quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -71,7 +71,6 @@ const config: QuartzConfig = {
       Plugin.ObsidianFlavoredMarkdown({ enableInHtmlEmbed: false }),
       Plugin.GitHubFlavoredMarkdown(),
       Plugin.TableOfContents({ collapseByDefault: true }),
-      Plugin.CrawlLinks({ markdownLinkResolution: "absolute", externalLinkIcon: false }),
       Plugin.Description(),
       Plugin.Latex({ renderEngine: "katex" }),
     ],
